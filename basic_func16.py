793bf998b0b38e0d7c003d4f319363b27cc2bbbd
Shoxrux Chulliev <chullievshoxrux0607@gmail.com>
2022-12-27 10:56:05 +0500

    16

--- basic_func16.py
+++ basic_func16.py
@@ -1,11 +1,14 @@
 def main(a, b):
     '''find the sum of a and b and return it.
-    
+
     Args:
         a (int): a number
         b (int): a number
-        
+
     Returns:
         int: the sum.
     '''
-    return
+    return a+b
+
+
+print(main(3, 2))
