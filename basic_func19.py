3416d2244a4321151a1655cf6088ed08f30c4ed0
Shoxrux Chulliev <chullievshoxrux0607@gmail.com>
2022-12-27 11:05:49 +0500

    19

--- basic_func19.py
+++ basic_func19.py
@@ -1,11 +1,14 @@
 def main(a, b):
     '''find the absolute value of the difference between a and b. Return it.
-    
+
     Args:
         a (int): a number
         b (int): a number
-        
+
     Returns:
         int: the result.
     '''
-    return
+    return abs(a-b)
+
+
+print(main(4, 11))
