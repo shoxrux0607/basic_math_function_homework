5cbf53ca4a407b242a5113a843577fb2083d7422
Shoxrux Chulliev <chullievshoxrux0607@gmail.com>
2022-12-27 10:52:45 +0500

    14

--- basic_func14.py
+++ basic_func14.py
@@ -1,11 +1,18 @@
+from math import floor
+
+
 def main(a, b):
     '''find the floor division of a and b and return it.
-    
+
     Args:
         a (int): a number
         b (int): a number
-        
+
     Returns:
         int: the result.
     '''
-    return 
+
+    return floor(a/b)
+
+
+print(main(13, 2))
