fd18b69285a60ccc1f1c4132a4e8bc32c3428b5e
Shoxrux Chulliev <chullievshoxrux0607@gmail.com>
2022-12-27 11:03:42 +0500

    18

--- basic_func18.py
+++ basic_func18.py
@@ -1,10 +1,17 @@
+from math import *
+
+
 def main(a):
     '''Assign the value pi to the parametr "a". Round the result to 2 decimal places and return it.
-    
+
     Args:
         a (float): a number
-        
+
     Returns:
         float: the result.
     '''
-    return
+    a = pi
+    return round(a, 2)
+
+
+print(main(3.1415))
