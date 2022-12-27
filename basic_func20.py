cc17b6acd4653acd1e18935393242ba8fe0c574f
Shoxrux Chulliev <chullievshoxrux0607@gmail.com>
2022-12-27 11:07:16 +0500

    20

--- basic_func20.py
+++ basic_func20.py
@@ -1,11 +1,15 @@
 def main(a, b):
     '''find the multiplication of a and b and return it.
-    
+
     Args:
         a (int): a number
         b (int): a number
-        
+
     Returns:
         int: the result.
     '''
-    return
+
+    return a*b
+
+
+print(main(5, 4))
