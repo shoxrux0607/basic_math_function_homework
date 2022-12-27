cc143e72f4d526540cc38e2c32a06306d72187b7
Shoxrux Chulliev <chullievshoxrux0607@gmail.com>
2022-12-27 10:46:21 +0500

    13

--- basic_func13.py
+++ basic_func13.py
@@ -1,10 +1,16 @@
+from math import sqrt
+
+
 def main(a):
     '''find the square root of a number and return it.
-    
+
     Args:
         a (int): a number
-        
+
     Returns:
         float: the absolute value.
     '''
-    return
+    return sqrt(a)
+
+
+print(main(9))
