51793d9c518addbe3ced47c36d06a6280bdcfcb8
Shoxrux Chulliev <chullievshoxrux0607@gmail.com>
2022-12-27 10:41:11 +0500

    01

--- basic_func11.py
+++ basic_func11.py
@@ -1,10 +1,14 @@
 def main(a):
     '''find the absolute value of a and return it
-    
+
     Args:
         a (int): a number
-        
+
     Returns:
         int: the absolute value.
     '''
-    return
+
+    return abs(a)
+
+
+print(main(-8))
