d3e3ed474d30021273ddfdb64de2cecd120c0242
Shoxrux Chulliev <chullievshoxrux0607@gmail.com>
2022-12-27 10:58:36 +0500

    17

--- basic_func17.py
+++ basic_func17.py
@@ -1,10 +1,17 @@
+from math import pi
+
+
 def main():
     '''return the value of pi
-    
+
     Args:
         a (float): a number
-        
+
     Returns:
         float: the value of pi.
     '''
-    return
+
+    return pi
+
+
+print(main())
